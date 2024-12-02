ba60d28dc4f7d6f2674ddbfc785b53bded5a8682
demirose41 <demirose41@gmail.com>
2024-12-01 22:53:18 -0800

    Completed practice

--- server/app.js
+++ server/app.js
@@ -5,5 +5,20 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.get('*', (req, res) => {
+  throw new Error(`Sorry, the request doesn't match`)
+})
+
+app.use((err, req, res, next) => {
+  err.status = 404;
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  console.log(err.message)
+  res.status(err.status || 500)
+  res.json({ 'message': err.message, "statusCode": err.status })
+})
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
